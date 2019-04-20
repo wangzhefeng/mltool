bf8cac2dbfe18af5266cb39591ded92ef6baee1a
wangzhefeng <wangzhefengr@163.com>
2019-04-20 17:32:50 +0800

    test

---
+++ demo/PCA_SVM_GridSearchCV.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+from sklearn.decomposition import PCA, NMF
+from sklearn.feature_selection import SelectKBest, chi2
+
+
+
+
